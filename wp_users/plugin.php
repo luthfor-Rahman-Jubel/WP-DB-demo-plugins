3adc8e86b76925d87238b79bf8b3e685241f547a
Jubel Ahmed <studentsleader44@gmail.com>
2021-06-11 16:27:07 +0600

    wp_users plugin

---
+++ wp_users/plugin.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Plugin Name: Transient Demo
+Plugin URI: https://jubel-ahemd.xyz
+Description: Demonstration of Transient API
+Version: 1.0.0
+Author: Jubel Ahmed
+Author URI: https://jubel-ahmed.xyz
+License: GPLv2 or later
+Text Domain: transient
+Domain Path: /languages/
+ */
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
